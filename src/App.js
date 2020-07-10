575a5e8fd333f5370e0d414bd02b246594248494
MalikNiazKhan <64806399+MalikNiazKhan@users.noreply.github.com>
2020-07-10 23:25:52 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -32,10 +32,10 @@ class App extends React.Component{
     return(
       <div className={styles.container}>
         <img className={styles.image} src={coronaImage} alt="COVID-19"/>
-<br>      
+      
         <CountryPicker handleCountryChange ={this.handleCountryChange}/>
       
-<br>      
+     
       <Grid container spacing={3}>
         <Grid item xs={12} sm={6}>
           <Cards data={data} />
